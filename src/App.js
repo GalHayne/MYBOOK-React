b6c239f943c602cb717b6964b9b847101be375f8
Gal <galhayne.gmail.com>
2023-02-20 21:24:06 +0200

    delete update book from db

--- src/App.js
+++ src/App.js
@@ -16,14 +16,16 @@ function App() {
         fetchBooks()
     }, [])
 
-    useEffect(() => {
-        console.log("hadar");
-    })
 
-    const editBookById = (id, newTitle) => {
+    const editBookById = async (id, newTitle) => {
+
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle,
+        });
+
         const updatedBook = books.map((book) => {
             if (book.id === id) {
-                return { ...book, title: newTitle }
+                return { ...book, ...response.data }
             }
             return book;
         })
@@ -31,7 +33,9 @@ function App() {
         setBooks(updatedBook);
     }
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+
+        await axios.delete(`http://localhost:3001/books/${id}`);
 
         const updatedBooks = books.filter((book) => {
             return book.id !== id;
