230c89f37c12b00bff15637623ef0c704d58bb4e
Gal <galhayne.gmail.com>
2023-02-20 22:04:15 +0200

    start to work with createContext

--- src/cmp/BookList.js
+++ src/cmp/BookList.js
@@ -1,10 +1,18 @@
 import BookShow from "./BookShow"
+import { useContext } from "react";
+import BooksContext from '../context/books';
 
 function BookList({ books, onDelete, onEdit }) {
+    const { count, incrementCount } = useContext(BooksContext);
+
+
+
     const renderBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />
     })
     return <div className="book-list">
+        {count}
+        <button onClick={incrementCount}>click me</button>
         {renderBooks}
     </div>
 }
