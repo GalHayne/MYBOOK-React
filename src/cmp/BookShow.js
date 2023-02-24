fd78ef0ba87c6a3bf56d8ab393575f9edf523130
Gal <galhayne.gmail.com>
2023-02-24 16:57:16 +0200

    fix bookEdit

--- src/cmp/BookShow.js
+++ src/cmp/BookShow.js
@@ -1,39 +1,42 @@
-import { useState } from "react";
-import BookEdit from "./BookEdit";
-import useBooksContext from "../hooks/use-books-context";
+import { useState } from 'react';
+import BookEdit from './BookEdit';
+import useBooksContext from '../hooks/use-books-context';
 
 function BookShow({ book }) {
     const [showEdit, setShowEdit] = useState(false);
     const { deleteBookById } = useBooksContext();
 
     const handleDeleteClick = () => {
-        deleteBookById(book.id)
+        deleteBookById(book.id);
     };
 
     const handleEditClick = () => {
         setShowEdit(!showEdit);
     };
 
-    const handleSumbit = () => {
-        setShowEdit(false)
-    }
+    const handleSubmit = () => {
+        setShowEdit(false);
+    };
 
-    let content = <h3>{book.title}</h3>
+    let content = <h3>{book.title}</h3>;
     if (showEdit) {
-        content = <BookEdit book={book} onEdit={handleSumbit} onSave={handleSumbit} />
+        content = <BookEdit onSubmit={handleSubmit} book={book} />;
     }
 
-
-    return <div className="book-show">
-        <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
-        <div>{content}</div>
-        <div className="actions">
-            <button className="edit" onClick={handleEditClick}>Edit</button>
-            <button className="delete" onClick={handleDeleteClick}>
-                Delete Me
-            </button>
+    return (
+        <div className="book-show">
+            <img alt="books" src={`https://picsum.photos/seed/${book.id}/300/200`} />
+            <div>{content}</div>
+            <div className="actions">
+                <button className="edit" onClick={handleEditClick}>
+                    Edit
+                </button>
+                <button className="delete" onClick={handleDeleteClick}>
+                    Delete
+                </button>
+            </div>
         </div>
-    </div>
+    );
 }
 
 export default BookShow;
